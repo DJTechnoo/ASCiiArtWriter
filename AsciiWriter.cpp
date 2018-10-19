d5756682c58245140543e54e924f3a0ebcd59d87
DJTechnoo <askeltechno@hotmail.com>
2018-10-19 16:48:43 +0200

    random color each line. Write to file

--- AsciiWriter.cpp
+++ AsciiWriter.cpp
@@ -1,24 +1,30 @@
 #include<iostream>
 #include<cstring>
-#include <thread>
-#include <chrono>
+#include<thread>
+#include<chrono>
+#include<Windows.h>
+#include<fstream>
+#include<vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 
 using namespace std;
 
 
+////////////////-->    CONSTS  <--////////////////////////
 const int MAXLINES = 6;			// ASCII art stacked horizontal lines per letter
-const int MAXMESSAGE = 5;
-const int STRLEN = 100;
-const int INPUTLEN = 20;		// Input Limit
-const int ASCIIVALUETOALPHABETINDEX = 97;	/* 
+const int MAXMESSAGE = 5;		// Number of message lines
+const int STRLEN = 100;			// message line length
+const int INPUTLEN = 200;		// Input Limit
+const int ASCIIVALUETOALPHABETINDEX = 97;	/*	Make this happen: foo['a'-97] == foo[0]
 											a - 97  =  0.  
 											b - 97  = 1 
 											etc...
 											*/
 
 
-
+////////////////-->    STRUCTS  <--////////////////////////
 struct Messages{
 	char messages[MAXMESSAGE][STRLEN] = {
 		{"hey wassup"},
@@ -28,6 +34,13 @@ struct Messages{
 		{"test"}
 	};
 };
+
+
+struct Input {
+	char in[INPUTLEN];
+};
+
+
 
 		// letter  |    line of letter  | char of line
 char alphabet[26]		[MAXLINES]			[9] = {
@@ -244,26 +257,65 @@ char alphabet[26]		[MAXLINES]			[9] = {
 };
 
 
+////////////////-->    GLOBAL FUNCTIONS  <--////////////////////////
 bool userWantsToContinue();
 void writeAsciiArtWith(char  input[]);
 void writeAsciiArtWithHappyBirthdayMessage(Messages & m);
 void getInput(char  input[], const char * msg);
+void writeAsciiWith(ofstream &outFile);
+void changeColor();
+void writtenInputToArt(Input &input);
+void init();
+void writeToFile();
 
 
+////////////////-->    GLOBAL VARIABLES  <--////////////////////////
+vector <Input> inputLines;
+HANDLE console;
+
+
+////////////////-->    MAIN   <--////////////////////////
 int main() {
 
-	//Messages msgs;
+	Messages bday;
+	Input input;
+
+	init();
 
 	do {
-		char input[INPUTLEN];
-		getInput(input, "Write something for ASCII art writing!");
-
-		writeAsciiArtWith(input);
-		//writeAsciiArtWithHappyBirthdayMessage(bday);
-
+		//writeAsciiArtWithHappyBirthdayMessage(bday);	// Use this one for predefined with "Message"	 
+		writtenInputToArt(input);
+		 
 	} while (userWantsToContinue());
 
+	writeToFile();
+	CloseHandle(console);
 	return 0;
+}
+
+
+
+void writeToFile()
+{
+	ofstream outFile("happybirthday.txt");
+	writeAsciiWith(outFile);
+}
+
+
+
+void init()
+{
+	console = GetStdHandle(STD_OUTPUT_HANDLE);
+	changeColor();
+}
+
+
+
+void writtenInputToArt(Input &input)
+{
+	getInput(input.in, "Write something for ASCII art writing!");
+	writeAsciiArtWith(input.in);					// Use this one to type each line in console
+	inputLines.push_back(input);		// save the current line
 }
 
 
@@ -284,10 +336,29 @@ void getInput(char  input[], const char * msg)
 }
 
 
+
+void writeAsciiWith(ofstream & outFile)
+{
+	for (int i = 0; i < inputLines.size(); i++) {
+		for (int line = 0; line < MAXLINES; line++) {
+			for (int letter = 0; letter < strlen(inputLines[i].in); letter++) {
+				if (inputLines[i].in[letter] - ASCIIVALUETOALPHABETINDEX < 0) outFile << "    ";
+				else {
+					outFile << alphabet[inputLines[i].in[letter] - ASCIIVALUETOALPHABETINDEX][line];
+				}
+				outFile << " ";
+			}
+			outFile << "\n";
+		}
+		outFile << "\n\n";
+	}
+}
+
+
 // Algorithm to write ascii art with our ascii art letters
 void writeAsciiArtWith(char  input[])
 {
-	char ch;
+
 	for (int line = 0; line < MAXLINES; line++) {
 		for (int letter = 0; letter < strlen(input); letter++) {
 			if (input[letter] - ASCIIVALUETOALPHABETINDEX < 0) cout << "    ";
@@ -299,6 +370,8 @@ void writeAsciiArtWith(char  input[])
 		}
 		this_thread::sleep_for(chrono::milliseconds(200));
 		cout << "\n";
+		//SetConsoleTextAttribute(console, line * 2);
+
 	}
 }
 
@@ -319,17 +392,30 @@ void writeAsciiArtWithHappyBirthdayMessage(Messages & m)
 			this_thread::sleep_for(chrono::milliseconds(200));
 			cout << "\n";
 		}
-		cout << "\n";
+		cout << "\n\n";
+		changeColor();
 	}
 }
 
 
 
 bool userWantsToContinue() {
-	cout << "\n\nQ = quit \n";
+	cout << "\n\n\tWRITE ANOTHER LINE? \n\t1=YES 0=DONE";
 	char ch;
 	cin >> ch; cin.ignore();
-	ch = toupper(ch);
-	if (ch != 'Q') return true;
+	if (ch != '0') {
+		changeColor();
+		return true;
+	}
 	return false;
+}
+
+
+
+void changeColor() {
+	srand(time(NULL));
+	int randomColor = rand() % 7 + 9;
+	while(randomColor == 12) randomColor = rand() % 7 + 9;
+	cout << "\n\tcolor = " << randomColor << '\n';
+	SetConsoleTextAttribute(console, randomColor);
 }
